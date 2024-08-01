125751206f4aaea3d9447379c8381435bab2ac98
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2024-08-01 12:28:08 +0630

    update: git init and next-pwa setup

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+	dest: "public",
+	disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+	// your other Next.js config options here
+});
+
+export default nextConfig;
