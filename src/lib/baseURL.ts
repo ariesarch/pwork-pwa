9bab12addf8b34f1e462bd4aa1ea8bb0e70890a8
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2024-08-07 12:38:57 +0630

    update: env, baseURL and axiosInstance

---
+++ src/lib/baseURL.ts
@@ -0,0 +1,3 @@
+export const backendURL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`;
+export const clientAccessToken = `${process.env.NEXT_PUBLIC_CLIENT_ACCESS_TOKEN}`;
+export const clientSecretToken = `${process.env.NEXT_PUBLIC_CLIENT_SECRET_TOKEN}`;
